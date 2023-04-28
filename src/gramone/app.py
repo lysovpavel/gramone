6d1496bc0fa50d335e900393764bc0440907166a
lysovpavel <lysovpavel@gmail.com>
2023-04-28 10:15:59 +0300

    bot refactor

--- src/gramone/app.py
+++ src/gramone/app.py
@@ -8,13 +8,13 @@ from gramone.views.message import message_router
 app = FastAPI()
 
 
-@app.on_event("startup")
-async def startup_event():
-    from bot.main import dp
-    executor = Executor(dp, skip_updates=True)
-    asyncio.ensure_future(executor._startup_polling())
-    asyncio.ensure_future(executor.dispatcher.start_polling(reset_webhook=True, timeout=20, relax=0.1, fast=True,
-                                                            allowed_updates=None))
+# @app.on_event("startup")
+# async def startup_event():
+#     from bot.main import dp
+#     executor = Executor(dp, skip_updates=True)
+#     asyncio.ensure_future(executor._startup_polling())
+#     asyncio.ensure_future(executor.dispatcher.start_polling(reset_webhook=True, timeout=20, relax=0.1, fast=True,
+#                                                             allowed_updates=None))
 
 
 app.include_router(message_router, prefix="/api/v1", tags=["messages"])
